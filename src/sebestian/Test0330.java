dc760e91e52bca4700e308962164362d78ef8b0d
Vetula_Mortem <renesebestian@gmail.com>
2017-03-30 14:40:21 +0200

    Test0330

---
+++ src/sebestian/Test0330.java
@@ -0,0 +1,11 @@
+package sebestian;
+
+public class Test0330 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i < 1;i++){
+			System.out.println("Guten Tag Hr. Sebestian !!!!");
+		}
+	}
+
+}
