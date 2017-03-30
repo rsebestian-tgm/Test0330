0063c957c9be3084c1a395a0e759193d3cace4cc
Vetula_Mortem <renesebestian@gmail.com>
2017-03-30 14:42:10 +0200

    5 fache Ausführung

--- src/sebestian/Test0330.java
+++ src/sebestian/Test0330.java
@@ -3,7 +3,7 @@ package sebestian;
 public class Test0330 {
 
 	public static void main(String[] args) {
-		for(int i = 0;i < 1;i++){
+		for(int i = 0;i < 5;i++){
 			System.out.println("Guten Tag Hr. Sebestian !!!!");
 		}
 	}
